76b59e458d6af80c7c9014ff3a3960092b814aa2
xcleung <xcleung0221@gmail.com>
2013-10-02 02:11:52 +0800

    C#实现的简易Socket

---
+++ C#/FirstSocket/SocketClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+//客户端程序
+namespace SocketClient
+{
+    class Program
+    {
+        private static byte[] result = new Byte[1024];
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");  //服务器地址
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 8889));
+                Console.WriteLine("连接服务器成功");
+            }
+            catch
+            {
+                Console.WriteLine("连接服务器失败，请按回车键退出");
+                return;
+            }
+
+            int receiveLength = clientSocket.Receive(result);
+            Console.WriteLine("接收服务器消息：{0}",Encoding.ASCII.GetString(result,0,receiveLength));
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                    string sendMessage = "hello i am lxc Client" + DateTime.Now;
+                    clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+                    Console.WriteLine("向服务器发送消息：{0}",sendMessage);
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    return;
+                }
+            }
+            Console.WriteLine("发送完毕，按回车键退出键");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/FirstSocket/SocketServer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+//服务器程序
+namespace SocketServer
+{
+    class Program
+    {
+        private static byte[] result = new Byte[1024];
+        private static int mypoot = 8889;
+        static Socket serverSocket;
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, mypoot));
+            serverSocket.Listen(10);
+            Console.WriteLine("启动监听{0}成功",serverSocket.LocalEndPoint.ToString());
+
+            Thread myThread = new Thread(ListenClientConnect);  //监听线程
+            myThread.Start();
+            Console.ReadLine();
+        }
+
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server say hello!"));
+                Thread reciveThread = new Thread(ReceiveMessage);
+                reciveThread.Start(clientSocket);
+            }
+        }
+
+        private static void ReceiveMessage(Object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    int receiveNumber = myClientSocket.Receive(result);
+                    Console.WriteLine("接收客户端{0}消息{1}",myClientSocket.RemoteEndPoint.ToString(),Encoding.ASCII.GetString(result,0,receiveNumber));
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    myClientSocket.Shutdown(SocketShutdown.Both);
+                    myClientSocket.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
